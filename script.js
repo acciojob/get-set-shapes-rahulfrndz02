ec3c6093f197b7a3ebd4b35aa1f37ca194f18f52
Rahul Kumar Pandit <55670332+rahulfrndz02@users.noreply.github.com>
2023-08-03 22:46:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
 //complete this code
-class Rectangle {}
+class Animal {
+	constructor(species){
+	this._species = species
+	}
+	get species(){
+	return this._species
+	}
+	makeSound(){
+		console.log("The Siamese makes a sound")
+	}
+}
 
-class Square extends Animal {}
+
+class Dog extends Animal {
+	bark(){
+		console.log("woof")
+	}
+}
+
+class Cat extends Animal {
+	purr(){
+		console.log("purr")
+	}
+}
+
+const animal = new Animal("Unknown");
+console.log(animal.species); // Output: Unknown
+animal.makeSound(); // Output: Some generic animal sound
+
+const cat = new Cat("Cat");
+console.log(cat.species); // Output: Cat
+cat.makeSound(); // Output: Some generic animal sound
+cat.purr(); // Output: purr
+
+const dog = new Dog("Dog");
+console.log(dog.species); // Output: Dog
+dog.makeSound(); // Output: Some generic animal sound
+dog.bark(); // Output: woof
+
+
+
+
+
+
+
 
 // Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
+window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
